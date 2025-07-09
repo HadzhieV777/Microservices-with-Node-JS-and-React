768f2d91ccab9c14a608533fc8d92987d080ad8a
HadzhieV777 <bdwhadjiev42@gmail.com>
2025-07-09 12:28:27 +0300

    Changes

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -13,14 +13,15 @@ app.get('/posts/:id/comments', (req, res) => {
 
 app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex');
-    const id = randomBytes(4).toString('hex');
     const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
 
     comments.push({ id: commentId, content });
 
-    res.status(201).send(comments[id])
+    commentsByPostId[req.params.id] = comments
+
+    res.status(201).send(comments)
 });
 
 app.listen(4001, () => {
