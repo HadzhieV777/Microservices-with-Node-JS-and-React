cba1f2e0c00db8206603d29e828c5b00529df264
HadzhieV777 <bdwhadjiev42@gmail.com>
2025-07-10 12:18:15 +0300

    Post list init

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 const PostList = () => {
     const [posts, setPosts] = useState({});
 
-    const fetchPosts = async () => {
+    const fetchData = async () => {
         const res = await axios.get('http://localhost:4000/posts');
 
         setPosts(res.data);
     };
 
     useEffect(() => {
-        fetchPosts();
+        fetchData();
     }, []);
 
     const renderedPosts = Object.values(posts).map(post => {
@@ -23,6 +24,7 @@ const PostList = () => {
 
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentList postId={post.id} /> 
                 <CommentCreate postId={post.id} />
             </div>
         </div>
