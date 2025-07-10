6ed9d013419b5c95a52e0b6b119809814ec0c94a
HadzhieV777 <bdwhadjiev42@gmail.com>
2025-07-10 11:32:17 +0300

    Posts list style

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -23,7 +23,7 @@ const PostCreate = () => {
                     onChange={e => setTitle(e.target.value)} 
                     className="form-control" />
             </div>
-            <button className="btn btn-primary">Submit</button>
+            <button className="btn btn-primary mt-3">Submit</button>
         </form>
     </div>;
 };
