46e8ddc847cea5924215737485a786d22f89d7b5
HadzhieV777 <bdwhadjiev42@gmail.com>
2025-07-14 12:48:47 +0300

    Parsing events

--- blog/query/index.js
+++ blog/query/index.js
@@ -7,15 +7,34 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
+const posts = {};
 
+app.get('/posts', (req, res) => {
+    res.send(posts);
 });
 
 app.get('/events', (req, res) => {
+    const { tyep, data } = req.body;
 
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+
+        posts[id] = { id, title, comments: [] };
+
+    };
+
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+
+        const post = posts[postId];
+
+        post.comments.push({ id, content })
+    };
+    
+    res.send({});
 });
 
-app.listen(4002, () => { 
+app.listen(4002, () => {
     console.log('Server is listening on port 4002...')
 }
 );
